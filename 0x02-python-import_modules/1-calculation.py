73d5f1f3482287f0a11f7776c9758a0e6a475eae
farhan <fcyare4study.com>
2022-09-24 01:43:49 -0700

    calculator

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print(f"{a} + {b} = {a+b}")
+    print(f"{a} - {b} = {a-b}")
+    print(f"{a} * {b} = {a*b}")
+    print(f"{a} / {b} = {a/b}")
