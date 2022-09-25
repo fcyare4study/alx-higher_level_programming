69e3aa2d358a8dc5bf0a088571f6abcaac21cf69
farhan <fcyare4study.com>
2022-09-25 13:07:54 -0700

    ispalindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,81 +1,73 @@
-include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
-  * is_palindrome - Checks if a singly linked list is a palindrome
-  * @head: The head of the singly linked list
-  *
-  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
-  */
-int is_palindrome(listint_t **head)
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
 {
-	listint_t *start = NULL, *end = NULL;
-	unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
+    const listint_t *current;
+    unsigned int n; /* number of nodes */
 
-	if (head == NULL)
-	return (0);
+    current = h;
+    n = 0;
+    while (current != NULL)
+    {
+        printf("%i\n", current->n);
+        current = current->next;
+        n++;
+    }
 
-if (*head == NULL)
-	return (1);
-
-	start = *head;
-	len = listint_len(start);
-	len_cyc = len * 2;
-	len_list = len_cyc - 2;
-	end = *head;
-
-	for (; i < len_cyc; i = i + 2)
-	{
-	if (start[i].n != end[len_list].n)
-	return (0);
-
-	len_list = len_list - 2;
-	}
-
-	return (1);
+    return (n);
 }
 
 /**
-  * get_nodeint_at_index - Gets a node from a linked list
-  * @head: The head of the linked list
-  * @index: The index to find in the linked list
-  *
-  * Return: The specific node of the linked list
-  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @n: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *current = head;
-	unsigned int iter_times = 0;
+    listint_t *new;
+    listint_t *current;
 
-	if (head)
-	{
-	while (current != NULL)
-{
-	if (iter_times == index)
-	return (current);
-	current = current->next;
-	++iter_times;
-}
-}
-	return (NULL);
+    current = *head;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = n;
+    new->next = NULL;
+
+    if (*head == NULL)
+        *head = new;
+    else
+    {
+        while (current->next != NULL)
+            current = current->next;
+        current->next = new;
+    }
+
+    return (new);
 }
 
 /**
-  * slistint_len - Counts the number of elements in a linked list
-  * @h: The linked list to count
-  *
-  * Return: Number of elements in the linked list
-  */
-size_t listint_len(const listint_t *h)
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
+ */
+void free_listint(listint_t *head)
 {
-	int lenght = 0;
+    listint_t *current;
 
-	while (h != NULL)
-	{
-	++lenght;
-	h = h->next;
-}
-
-	return (lenght);
-}
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current);
+    }
